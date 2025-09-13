ca0e65d94fd8fef894eac9f22eb89b6432e6b2fe
fjoelnir <thrudhamar@web.de>
2025-09-13 20:10:49 +0200

    ci/lint: fix ESLint config to CJS; ignore Ruff I001 in tests

--- bot-gateway/.eslintrc.cjs
+++ bot-gateway/.eslintrc.cjs
@@ -1,13 +1,12 @@
 /* eslint-env node */
-export default {
+module.exports = {
   root: true,
   env: { es2023: true, node: true },
   extends: ['standard'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   rules: {
-    // projektfreundliche Defaults
     'no-console': 'off',
     'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
   },
   ignorePatterns: ['node_modules/', 'dist/']
-}
+};
